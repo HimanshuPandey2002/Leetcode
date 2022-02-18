bc8d47681ac778742033ae286ed7b8db87de1570
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-02-18 14:27:10 +0530

    Time: 7 ms (51.79%), Space: 43.2 MB (40.61%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,17 +1,16 @@
 class Solution {
-    public static boolean check(String s){
-        int i = 0, j = s.length() - 1;
-        while(i <= j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++; j--;
-        }
-        return true;
+    public static boolean isAlNum(char c){
+        return (Character.isDigit(c) || Character.isLetter(c));
     }
     public boolean isPalindrome(String s) {
-        String str = "";
-        for(char c: s.toLowerCase().toCharArray())
-            if(Character.isLetter(c) || Character.isDigit(c))
-                str += c;
-        return check(str);
+        s = s.toLowerCase();
+    
+        for(int i = 0, j = s.length() - 1; i < j ; ){
+            if(!isAlNum(s.charAt(i))) i++;
+            else if(!isAlNum(s.charAt(j))) j--;
+            else if(s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        
+        return true;
     }
 }
