c1d43c6751152bb03d216e9ffc67b910f1021956
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-03-03 22:31:02 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (20.08%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,18 +1,16 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         int n = nums.length;
-        
         if(n < 3) return 0;
+        int count = 0;
         
-        int i=0,j=0,cn=n,count=0;
-        while(i<n-1){
-            j=i+1;
-            int d = nums[j]-nums[i];
-            while(j<n && nums[j]-nums[j-1] == d){
-                if(j-i+1>=3) count++;
+        for(int i = 0; i < n-1; i++){
+            int j = i+1;
+            int diff = nums[j] - nums[i];
+            while(j < n && nums[j] - nums[j-1] == diff){
+                if(j - i + 1 >= 3) count++;
                 j++;
             }
-            i++;
         }
         return count;
     }
