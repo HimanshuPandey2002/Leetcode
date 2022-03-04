b1d814fcd57e31174a798800a74b9bd3660fd6ef
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-03-04 21:58:41 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (67.28%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean checkStraightLine(int[][] coordinates) {
+    public boolean checkStraightLine(int[][] p) {
 //         int dx = p[1][0] - p[0][0];
 //         int dy = p[1][1] - p[0][1];
         
@@ -8,12 +8,12 @@ class Solution {
 //                 return false;
 //         }
 //         return true;
-        int x0 = coordinates[0][0], y0 = coordinates[0][1], 
-            x1 = coordinates[1][0], y1 = coordinates[1][1];
-        int dx = x1 - x0, dy = y1 - y0;
-        for (int[] co : coordinates) {
+        int dx = p[1][0] - p[0][0];
+        int dy = p[1][1] - p[0][1];
+        
+        for (int[] co : p) {
             int x = co[0], y = co[1];
-            if (dx * (y - y1) != dy * (x - x1))
+            if (dx * (y - p[1][1]) != dy * (x -  p[1][0]))
                 return false;
         }
         return true;
