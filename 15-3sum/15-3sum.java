1077c5beacf0a175651578133d7f108752da15c4
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-03-26 11:22:43 +0530

    Time: 21 ms (88.79%), Space: 46.6 MB (85.14%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -5,14 +5,13 @@ class Solution {
         for (int i = 0; i < nums.length-1; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i + 1, k = nums.length - 1;  
-            int target = -nums[i];
-            
+
             while (j < k) {
-                if (nums[j] + nums[k] == target) {
+                if (nums[j] + nums[k] == -nums[i]) {
                     res.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
                     while (j < k && nums[j] == nums[j - 1]) j++;
                     while (j < k && nums[k] == nums[k + 1]) k--;
-                } else if (nums[j] + nums[k] > target)k--;
+                } else if (nums[j] + nums[k] > -nums[i]) k--;
                 else j++;
             }
         }
