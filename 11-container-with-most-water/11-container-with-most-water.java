e24c10a31cb8eab89ae03b40a074c2f64bc35f07
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-03-26 10:02:34 +0530

    Time: 164 ms (5.63%), Space: 81.8 MB (13.17%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int max = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = n-1; j >= i; j--){
+                int val = Math.min(height[i], height[j]) * Math.abs(j-i);
+                if(val > max)
+                    max = val;
+                if(height[i] < height[j]) break;
+            }
+        }
+        return max;
+    }
+}
