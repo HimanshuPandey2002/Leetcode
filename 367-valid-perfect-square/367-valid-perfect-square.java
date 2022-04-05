7af2f5c006b0beee60241637e2b9c5cfc3cddd40
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-04-05 20:50:08 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (31.41%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean isPerfectSquare(int n) {
-        int num = (int)Math.sqrt(n);
-        return num * num == n; 
+        int start = 0, end = n/2;
+        
+        if(n == 1) return true;
+        
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+            if(mid * mid == n)
+                return true;
+            else if(mid * mid > n)
+                end = (int)mid-1;
+            else
+                start = (int)mid+1;
+        }
+        return false;
     }
 }
