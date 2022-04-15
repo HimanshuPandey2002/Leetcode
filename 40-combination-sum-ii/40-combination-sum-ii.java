953190e8ac8ef4ac94ef56654756db9c1c655443
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-04-15 16:15:22 +0530

    Time: 2 ms (99.93%), Space: 42.4 MB (91.45%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void help(int[] arr, int ind, int target, List<List<Integer>> res, List<Integer> list){
+        // if(target < 0 || ind > arr.length) return;
+        // if(ind == arr.length){
+        if(target == 0){
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        
+        // if(target < arr[ind]){
+            for(int i = ind; i < arr.length; i++){
+                if(i > ind && arr[i] == arr[i-1]) continue;
+                if(arr[i] > target) break;
+                list.add(arr[i]);
+                help(arr, i+1, target-arr[i], res, list);
+                list.remove(list.size()-1);
+            }
+        // }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        help(candidates, 0, target, result, new ArrayList<Integer>());
+        return result;
+    }
+}
