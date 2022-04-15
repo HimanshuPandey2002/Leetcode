0525c21eca0531bcc7eb1999bdcda1401c33831e
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-04-15 11:08:54 +0530

    Time: 2 ms (97.27%), Space: 42.8 MB (85.82%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,15 +1,27 @@
 class Solution {
     public static void help(int[] arr, int ind, int target, List<Integer> list, List<List<Integer>> res){
-        if(target < 0 || ind >= arr.length) return;
-        if(target == 0){
-            res.add(new ArrayList<Integer>(list));
+//         if(target < 0 || ind >= arr.length) return;
+//         if(target == 0){
+//             res.add(new ArrayList<Integer>(list));
+//             return;
+//         }
+        
+//         list.add(arr[ind]);
+//         help(arr, ind, target - arr[ind], list, res);
+        
+//         list.remove(list.size()-1);
+//         help(arr, ind+1, target, list, res);
+        if(ind == arr.length){
+            if(target == 0)
+                res.add(new ArrayList<Integer>(list));
             return;
         }
         
-        list.add(arr[ind]);
-        help(arr, ind, target - arr[ind], list, res);
-        
-        list.remove(list.size()-1);
+        if(arr[ind] <= target){
+            list.add(arr[ind]);
+            help(arr, ind, target-arr[ind], list, res);
+            list.remove(list.size()-1);
+        }
         help(arr, ind+1, target, list, res);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
