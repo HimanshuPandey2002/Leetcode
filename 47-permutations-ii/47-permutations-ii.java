fb32f3b1fdabc44be06ce52a7e1feb8f918d39b0
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-04-17 09:18:06 +0530

    Time: 2 ms (91.20%), Space: 43.2 MB (80.18%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,45 +1,31 @@
 class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        help(nums, 0, res);
+        return res;
+    }
+    
     private void swap(int i, int j, int[] arr){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
     
-//     public void help(int[] arr, int ind, List<List<Integer>> res){
-//         if(ind == arr.length){
-//             ArrayList<Integer> list = new ArrayList<Integer>();
-//             for(int i = 0; i < arr.length; i++)
-//                 list.add(arr[i]);
-//             if(!res.contains(list))
-//                 res.add(list);
-//             return ;
-//         }
-        
-//         for(int i = ind; i < arr.length; i++){
-//             swap(i, ind, arr);
-//             help(arr, ind+1, res);
-//             swap(i, ind, arr);
-//         }
-//     }
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        permute(ans, nums, 0);
-        return ans;
-    }
-    
-    private void permute(List<List<Integer>> ans, int[] nums, int index) {
-        if (index == nums.length) { 
-            List<Integer> temp = new ArrayList<>();
-            for (int num: nums) { temp.add(num); }
-            ans.add(temp);
-            return;
+    public void help(int[] arr, int ind, List<List<Integer>> res){
+        if(ind == arr.length){
+            List<Integer> list = new ArrayList<Integer>();
+            for(int i: arr) list.add(i);
+            res.add(list);
+            return ;
         }
-        Set<Integer> appeared = new HashSet<>();
-        for (int i=index; i<nums.length; ++i) {
-            if (appeared.add(nums[i])) {
-                swap(index, i, nums);
-                permute(ans, nums, index+1);
-                swap(index, i, nums);
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = ind; i < arr.length; i++){
+            if(set.add(arr[i])){
+                swap(i, ind, arr);
+                help(arr, ind+1, res);
+                swap(i, ind, arr);   
             }
         }
     }
