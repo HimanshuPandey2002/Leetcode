af550e2e28f039aed34e81585e69fbab41ec0ae6
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-05-25 22:35:54 +0530

    Time: 77 ms (41.36%), Space: 77.1 MB (87.60%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,28 +1,15 @@
 class Solution {
-    public int maxEnvelopes(int[][] envelopes) {
-        if(envelopes == null 
-           || envelopes.length == 0 
-           || envelopes[0] == null 
-           || envelopes[0].length != 2)
-            return 0;
-        Arrays.sort(envelopes, new Comparator<int[]>(){
-            public int compare(int[] arr1, int[] arr2){
-                if(arr1[0] == arr2[0])
-                    return arr2[1] - arr1[1];
-                else
-                    return arr1[0] - arr2[0];
-           } 
-        });
-        int dp[] = new int[envelopes.length];
-        int len = 0;
-        for(int[] envelope : envelopes){
-            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
-            if(index < 0)
-                index = -(index + 1);
-            dp[index] = envelope[1];
-            if(index == len)
-                len++;
+     public int maxEnvelopes(int[][] E) {
+        Arrays.sort(E, (a,b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int[] dp = new int[E.length];
+        int ans = 0;
+        for (int[] env : E) {
+            int height = env[1];
+            int left = Arrays.binarySearch(dp, 0, ans, height);
+            if (left < 0) left = -left - 1;
+            if (left == ans) ans++;
+            dp[left] = height;
         }
-        return len;
+        return ans;
     }
 }
