869f9b1805775a1e6397da3ce14402002161cc67
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-06-02 08:22:10 +0530

    Time: 32 ms (12.41%), Space: 145.1 MB (5.83%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -9,18 +9,20 @@ class Solution {
             for(int j = 0; j < m; j++)
                 arr[j][i] = box[n-i-1][j];
         
-        for (int i = m - 1; i >= 0; i--) {
-             for (int j = 0; j < n; j++) {
-                 if (arr[i][j] == '#') {
-                     int curRow = i;
-                     while (curRow + 1 < m && arr[curRow + 1][j] == '.') curRow++;
-                     if (curRow != i) {
-                         arr[curRow][j] = '#';
-                         arr[i][j] = '.';
-                     }
-                 }
-             }
-         }
+        for(int i = m-1; i >= 0; i--){
+            for(int j = 0; j < n; j++){
+                if(arr[i][j] == '#'){
+                    int currRow = i;
+                    
+                    while(currRow + 1 < m && arr[currRow+1][j] == '.') currRow++;
+                    
+                    if(currRow != i){
+                        arr[currRow][j] = '#';
+                        arr[i][j] = '.';
+                    }
+                }
+            }
+        }
         
         return arr;
     }
