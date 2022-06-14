83231ae4f526f8ead2b062f79c130fd0dc8927a7
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-06-14 10:19:51 +0530

    Time: 14 ms (51.72%), Space: 46.6 MB (65.75%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,23 +1,26 @@
 class Solution {
-    int lcs( char[] X, char[] Y, int m, int n ) {
-        int L[][] = new int[m+1][n+1];
+    static int lcs(String X,String Y,int m,int n,int[][] dp){
+        if (m == 0 || n == 0) return 0;
+        if (dp[m][n] != -1) return dp[m][n];
 
-        for (int i=0; i<=m; i++) {
-            for (int j=0; j<=n; j++) {
-                if (i == 0 || j == 0)
-                    L[i][j] = 0;
-                else if (X[i-1] == Y[j-1])
-                    L[i][j] = L[i-1][j-1] + 1;
-                else
-                    L[i][j] = Math.max(L[i-1][j], L[i][j-1]);
-            }
+        if(X.charAt(m - 1) == Y.charAt(n - 1)){
+          dp[m][n] = 1 + lcs(X, Y, m - 1, n - 1, dp);
+          return dp[m][n];
         }
-        return L[m][n];
+
+        dp[m][n] = Math.max(lcs(X, Y, m, n - 1, dp),lcs(X, Y, m - 1, n, dp));
+        return dp[m][n];
     }
     
     public int minDistance(String word1, String word2) {
-        int n = word1.length(), m = word2.length();
-        int len = lcs(word1.toCharArray(), word2.toCharArray() , n, m);
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for(int i = 0; i < m + 1; i++)
+          for(int j = 0; j < n + 1; j++)
+            dp[i][j] = -1;
+        
+        int len = lcs(word1, word2 , m, n, dp);
         return (n-len) + (m-len);
     }
 }
