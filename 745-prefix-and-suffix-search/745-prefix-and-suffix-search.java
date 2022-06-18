f196ebbc4b48b7522740cc4f2abc94eaf84cc55e
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-06-18 11:01:42 +0530

    Time: 1393 ms (7.79%), Space: 289.2 MB (5.00%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -1,13 +1,16 @@
 class WordFilter {
     HashMap<String, Integer> map = new HashMap<>();
+    
+    public WordFilter(String[] words){
+        int ind = 0;
+        
+        for(String word : words){
+            int n = word.length();
 
-    public WordFilter(String[] words) {
-        for(int w = 0; w < words.length; w++){
-            for(int i = 0; i <= 10 && i <= words[w].length(); i++){
-                for(int j = 0; j <= 10 && j <= words[w].length(); j++){
-                    map.put(words[w].substring(0, i) + "#" + words[w].substring(words[w].length()-j), w);
-                }
-            }
+            for(int i = 0; i <= 10 && i <= n; i++)
+                for(int j = 0; j <= 10 && j <= n; j++)
+                    map.put(word.substring(0,i) + "#" + word.substring(n - j), ind);
+            ind++;
         }
     }
 
