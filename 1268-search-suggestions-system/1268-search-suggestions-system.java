a7e80d431ee9acb647d32ba000856dd33a5ee51c
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-06-19 08:43:58 +0530

    Time: 59 ms (51.11%), Space: 59.2 MB (69.75%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        PriorityQueue<String> q = new PriorityQueue<>(3, (s1, s2) -> s1.compareTo(s2));
+        List<List<String>> result = new ArrayList<>();
+        Arrays.sort(products);
+        
+        for(int i = 1; i <= searchWord.length(); i++){
+            String str = searchWord.substring(0, i);
+            
+            for(String s: products)
+                if(s.startsWith(str))
+                    q.offer(s);
+            
+            List<String> list = new ArrayList<>();
+            
+            for(int j = 0; j < 3; j++)
+                if(q.peek() != null)
+                    list.add(q.poll());
+            
+            q.clear();
+            result.add(list);
+        }
+        
+        return result;
+    }
+}
