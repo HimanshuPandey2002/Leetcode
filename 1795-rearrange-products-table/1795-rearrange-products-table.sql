8f7121d8512947c4c013099bc818dd31b922e7f5
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-06-26 22:32:34 +0530

    Time: 959 ms (11.71%), Space: 0B (100.00%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,5 @@
+SELECT product_id,'store1' as store, store1 as price FROM products WHERE store1 IS NOT null
+UNION
+SELECT product_id,'store2' as store, store2 as price FROM products WHERE store2 IS NOT null
+UNION
+SELECT product_id,'store3' as store, store3 as price FROM products WHERE store3 IS NOT null
