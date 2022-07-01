e1d91074a231322e8d2f1a69a6ff1b299bd4d700
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-01 18:03:27 +0530

    Time: 1354 ms (14.65%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,5 +1,5 @@
 SELECT u.name, (SELECT ifnull(SUM(r.distance), 0)
 FROM rides r
 WHERE u.id = r.user_id) as travelled_distance
-from users u
-order by travelled_distance desc, name asc
+FROM users u
+ORDER BY travelled_distance desc, name asc
