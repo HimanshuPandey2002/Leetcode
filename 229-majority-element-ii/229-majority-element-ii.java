61a21648d05f23727d5550a598a975aad0881ae6
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-06 00:08:32 +0530

    Time: 3 ms (80.90%), Space: 49.1 MB (78.59%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,14 +1,34 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<Integer>();
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        List<Integer> list = new ArrayList<>();
+        int c1 = 0, c2 = 0;
+        int e1 = -1, e2 = -1;
         
-        for(int i: nums)
-            map.merge(i, 1, Integer::sum);
+        for(int i : nums){
+            if(i == e1) c1++;
+            else if(i == e2) c2++;
+            else if(c1 == 0){
+                e1 = i;
+                c1 = 1;
+            } else if(c2 == 0){
+                e2 = i;
+                c2 = 1;
+            } else {
+                c1--;
+                c2--;
+            }
+        }
+        c1 = c2 = 0;
+        for(int i : nums){
+            if(i == e1) c1++;
+            if(i == e2) c2++;
+        }
         
-        for(Map.Entry<Integer, Integer> entry: map.entrySet())
-            if(entry.getValue() > nums.length/3)
-                list.add(entry.getKey());
+        if(c1 > nums.length/3)
+            list.add(e1);
+        if(c2 > nums.length/3)
+            if(!list.contains(e2))
+            list.add(e2);
         
         return list;
     }
