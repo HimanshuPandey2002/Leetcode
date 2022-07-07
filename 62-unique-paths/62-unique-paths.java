b64e6953e5a74cda1a5b0bc6e700ea2f6f10adf4
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-07 12:28:00 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (69.80%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,19 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[n][m];
+        int N = n + m - 2;
+        int r = m - 1;
+        double res = 1;
         
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                dp[i][j] = -1;
+        for(int i = 1; i <= r; i++)
+            res = res * (N - r + i) / i;
         
-        return countPaths(0, 0, n, m, dp);
+        return (int)res;
     }
     
-    public int countPaths(int i, int j, int n, int m, int[][] dp){
-        if((i >= n) || (j >= m)) return 0;
-        if((i == n-1) && (j == m-1)) return 1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        else return dp[i][j] = countPaths(i+1, j, n, m, dp) + countPaths(i, j+1, n, m, dp);
-    }
 }
