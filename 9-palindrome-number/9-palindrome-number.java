08a59b7d0177a80ac24a228f44127b16b18d2dfd
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-11 23:05:50 +0530

    Time: 18 ms (28.73%), Space: 45.1 MB (36.68%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -3,12 +3,10 @@ class Solution {
         String s = String.valueOf(x);
         int i = 0, j = s.length() - 1;
         
-        while(i <= j){
+        while(i <= j)
             if(s.charAt(i++) != s.charAt(j--))
                 return false;
-            // j--;
-            // i++;
-        }
+    
         return true;
     }
 }
