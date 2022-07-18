c1e5a01cb269c595e7a32709dc857d04b0c15440
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-18 23:33:41 +0530

    Time: 10 ms (98.86%), Space: 166.2 MB (34.40%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        long sum = 0;
+        int[] rad = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            if(i < k) rad[i] = -1;
+            sum += nums[i];
+            
+            if(i >= 2*k){
+                rad[i-k] = (int)((double)sum/(double)(2*k+1));
+                sum -= nums[i-2*k]; 
+            }
+            
+            if(i >= n-k) rad[i] = -1;
+        }
+        return rad;
+    }
+}
