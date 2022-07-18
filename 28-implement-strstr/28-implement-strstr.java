25ffc4c568f8c5d5f9be189c1add0072a2f35d14
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-18 16:41:14 +0530

    Time: 1 ms (64.96%), Space: 41.5 MB (79.95%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String h, String n) {
+        if(h.length() == 0) return -1;
+        if(n.length() == 0) return 0;
+        
+        for (int i = 0; i < h.length(); i++) {
+            if (i + n.length() > h.length()) break;
+            
+            for (int j = 0; j < n.length(); j++) {
+                if (h.charAt(i+j) != n.charAt(j))
+                    break;
+                if (j == n.length()-1)
+                    return i;
+            }
+        }
+        
+        return -1;
+    }
+}
