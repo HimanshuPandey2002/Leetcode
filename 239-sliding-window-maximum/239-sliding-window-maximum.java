b5d5a29c7ef25026907d161a95253706d7de1ffb
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-21 23:10:36 +0530

    Time: 427 ms (6.41%), Space: 149.2 MB (49.17%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] A, int k) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int j = 0, i = 0; j < A.length; j++){
+            while(list.size() > 0 && list.get(list.size()-1) < A[j]){
+                list.remove(list.size()-1);
+            }
+            
+            list.add(A[j]);
+            
+            if(j-i+1 == k){
+                ans.add(list.get(0));
+                if(list.get(0) == A[i++])
+                    list.remove(0);
+            }
+        }
+        
+        
+        return ans.stream().mapToInt(l -> l).toArray();
+    }
+}
