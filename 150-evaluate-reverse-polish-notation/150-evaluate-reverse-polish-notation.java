659bd533472a90ae212ad9a629a2a02869c330f7
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-07-23 10:12:49 +0530

    Time: 10 ms (44.16%), Space: 43.6 MB (77.31%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -3,23 +3,21 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         
         for(String s: tokens){
-            if(s.equals("*") || s.equals("/") || s.equals("+") || s.equals("-")){
-                switch(s){
-                    case "+":
-                        st.push(st.pop() + st.pop());
-                        break;
-                    case "-":
-                        st.push(-st.pop() + st.pop());
-                        break;
-                    case "*":
-                        st.push(st.pop() * st.pop());
-                        break;
-                    case "/":
-                        st.push((int)((double)1/((double)st.pop() / (double)st.pop())));
-                        break;
-                }
-            } else {
-                st.push(Integer.parseInt(s));
+            switch(s){
+                case "+":
+                    st.push(st.pop() + st.pop());
+                    break;
+                case "-":
+                    st.push(-st.pop() + st.pop());
+                    break;
+                case "*":
+                    st.push(st.pop() * st.pop());
+                    break;
+                case "/":
+                    st.push((int)((double)1/((double)st.pop() / (double)st.pop())));
+                    break;
+                default:
+                    st.push(Integer.parseInt(s));
             }
         }
         return st.peek();
