dbcaf6a0c1c5953b16e110738fec3071eeacfbbb
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-08-31 15:31:25 +0530

    Time: 104 ms (11.80%), Space: 68.8 MB (39.14%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -1,68 +1,71 @@
 class Node {
-    Node[] links = new Node[26];
-    boolean flag = false;
-    
-    boolean contains(char c){
-        return links[c - 'a'] != null;
-    }
-    
-    void add(char c, Node node){
-        links[c - 'a'] = node;
-    }
-    
-    Node get(char c){
-        return links[c - 'a'];
-    }
-    
-    void setEnd(){
-        flag = true;
-    }
-    
-    boolean isEnd(){
-        return flag;
-    }
+	Node[] links = new Node[26];
+	boolean flag = false;
+
+	boolean contains(char c){
+		return links[c - 'a'] != null;
+	}
+
+	Node get(char c){
+		return links[c - 'a'];
+	}
+
+	void add(char c, Node node){
+		links[c - 'a'] = node;
+	}
+
+	void setEnd(){
+		flag = true;
+	}
+
+	boolean isEnd(){
+		return flag;
+	}
+
 }
+
 
 class Trie {
     private static Node root;
 
-    public Trie() {
-        root = new Node();
-    }
-    
-    public void insert(String word) {
-        Node node = root;
-        
-        for(char c: word.toCharArray()){
-            if(!node.contains(c))
-                node.add(c, new Node());
-            node = node.get(c);
-        }
-        
-        node.setEnd();
-    }
-    
-    public boolean search(String word) {
-        Node node = root;
-        
-        for(char c: word.toCharArray()){
-            if(!node.contains(c))
-                return false;
-            node = node.get(c);
-        }
-        return node.isEnd();
-    }
-    
-    public boolean startsWith(String prefix) {
-        Node node = root;
-        
-        for(char c: prefix.toCharArray()){
-            if(!node.contains(c))
-                return false;
-            node = node.get(c);
-        }
-        return true;
-    }
+	Trie(){
+		root = new Node();
+	}
+
+	public void insert(String word){
+		Node node = root;
+
+		for(int i = 0; i < word.length(); i++){
+			char c = word.charAt(i);
+			if(!node.contains(c))
+				node.add(c, new Node());
+			node = node.get(c);
+		}
+
+		node.setEnd();
+	}
+
+	public boolean search(String word){
+		Node node = root;
+
+		for(int i = 0; i < word.length(); i++){
+			char c = word.charAt(i);
+			if(!node.contains(c)) return false;
+			node = node.get(c);
+		}
+		return node.isEnd();
+	}
+
+	public boolean startsWith(String prefix){
+		Node node = root;
+
+		for(int i = 0; i < prefix.length(); i++){
+			char c = prefix.charAt(i);
+			if(!node.contains(c)) return false;
+			node = node.get(c);
+		}
+		return true;
+	}
 }
 
 /**
