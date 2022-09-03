1592291e16111fb4f28cdb39f0f3cfaf3807ff5a
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-09-03 10:50:49 +0530

    Time: 1 ms (89.91%), Space: 41.5 MB (99.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -9,9 +9,7 @@ class Solution {
     }
     
     public int minCost(int[] cost, int n){
-        if(n < 0) return 0;
-        if(n == 0 || n == 1) return cost[n];
-        
+        if(n <= 1) return cost[n];
         if(dp[n] != -1) return dp[n];
         
         return dp[n] = cost[n] + Math.min(minCost(cost, n-1), minCost(cost, n-2));
