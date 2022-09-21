1e893c2aea0fcd595de152811f98909f0664fc4d
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-09-21 10:12:39 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (30.60%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,20 +1,37 @@
 class Solution {
-    public int rob(int[] arr) {
-        int n = arr.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+//     public int rob(int[] arr) {
+//         int n = arr.length;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
         
-        dp[0] = arr[0];
+//         dp[0] = arr[0];
+        
+//         for(int i = 1; i < n; i++){
+//             int take = arr[i];
+            
+//             if(i > 1)
+//                 take += dp[i-2];
+                
+//             int not = dp[i-1];
+//             dp[i] = Math.max(take, not);
+//         }
+//         return dp[n-1];
+//     }
+    
+    public int rob(int[] arr){
+        int n = arr.length;
+        int prev = arr[0];
+        int prev2 = 0;
         
         for(int i = 1; i < n; i++){
-            int take = arr[i];
-            
-            if(i > 1)
-                take += dp[i-2];
-                
-            int not = dp[i-1];
-            dp[i] = Math.max(take, not);
+            // int take = arr[i];
+            // if(i > 1) take += prev2;
+            int take = arr[i] + (i > 1 ? prev2 : 0);
+            int not = prev;
+            int curr = Math.max(not, take);
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n-1];
+        return prev;
     }
 }
