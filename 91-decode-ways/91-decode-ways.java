289f2970c8f2c25ad607dd8cd459e9eda7631877
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-09-29 10:19:49 +0530

    Time: 2 ms (69.69%), Space: 42.1 MB (73.29%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,24 @@
+class Solution {
+    int dp[];
+    
+    public int numDecodings(String s) {
+        dp = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return solve(0, s);
+    }
+    
+    public int solve(int ind, String s){
+        if(ind == s.length()) return 1;
+        if(s.charAt(ind) == '0') return 0;
+        
+        if(dp[ind] != -1) return dp[ind];
+        
+        int one = solve(ind+1, s);
+        int two = 0;
+        
+        if(ind < s.length()-1 && Integer.parseInt(s.substring(ind, ind+2)) <= 26)
+            two = solve(ind+2, s);
+        
+        return dp[ind] = one + two;
+    }
+}
