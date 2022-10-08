ceef033f9024165820fb1b8ca881f13e7f0fc251
Himanshu pandey <58338799+HimanshuPandey2002@users.noreply.github.com>
2022-10-08 12:00:26 +0530

    Time: 1100 ms (9.65%), Space: 144.3 MB (5.57%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -7,19 +7,16 @@ class Solution {
     public int solve(int[] cuts, int l, int r, HashMap<ArrayList<Integer>, Integer> map){
         ArrayList<Integer> list = new ArrayList<>();
         
-        list.add(l);
-        list.add(r);
+        list.add(l); list.add(r);
         
         if(map.containsKey(list)) return map.get(list);
         
         int min = Integer.MAX_VALUE;
         
         for(int i: cuts){
-            if(i > l && i < r){
-                int temp = solve(cuts, l, i, map);
-                temp += solve(cuts, i, r, map);
-                min = Math.min(temp + r - l, min);
-            }
+            if(i <= l || i >= r) continue;
+            int temp = solve(cuts, l, i, map) + solve(cuts, i, r, map);
+            min = Math.min(temp + r - l, min);
         }
         
         if(min == Integer.MAX_VALUE) min = 0;
